2ca79d032245741b161ca53f22d186a739e3a7f5
Caitlin Elliott <ceelliott90@gmail.com>
2023-11-21 14:29:08 -0500

    mobile carousel nav

--- app.js
+++ app.js
@@ -62,94 +62,6 @@ sections.forEach(section => {
     observer.observe(section)
 })
 
-// PIE
-
-// google.charts.load('current', {'packages':['corechart']});
-// google.charts.setOnLoadCallback(drawChart);
-
-// function drawChart() {
-
-//     var data = new google.visualization.DataTable();
-
-//     data.addColumn('string', 'Role');
-//     data.addColumn('number', 'Hours');
-//     data.addColumn({type: 'string', role: 'tooltip', 'p': {'html': true}});
-
-//     data.addRows([
-//         ['Developer', 60,`
-        
-        
-
-//         <div class="expandable" id="developer-tooltip-body">
-//         <p>I've never got over the magic of bringing a design to life via lines of code, and I hope I never do.</p>
-
-//         <p>I've worked on projects at a variety of scales, from marketing websites for small businesses to enterprise-level web tools and mobile apps.
-//         </p>
-
-
-//     </div>
-
-//     <div class="tooltip-header">
-//         <div class="tooltip-title designer-tooltip"><span class="material-symbols-outlined tooltip-icon">devices</span> 60% Developer</div>
-//         <button onclick="expand('developer')" class="expand-btn"><span class="material-symbols-outlined">expand_content</span></button>
-//         </div>
-
-//         `],
-//         ['Designer', 40,
-//         `
-//         <div class="tooltip-header">
-//         <div class="tooltip-title dev-tooltip"><span class="material-symbols-outlined tooltip-icon">format_color_fill</span>40% Designer</div>
-//         <button onclick="expand('designer')" class="expand-btn"><span class="material-symbols-outlined">expand_content</span></button>
-//         </div>
-
-
-
-
-//     </div>
-
-//         `],
-//       ]);    
-//     var options = {
-//         title: '',
-//         legend: 'none',
-//         backgroundColor: 'transparent',
-//         chartArea: {
-//             height: '100%',
-//             width: '100%',
-//             top: 10,
-//             left: 0,
-//             bottom: 10,
-//             right: 0
-//         },
-//         colors: ['#29303a', '#566b57'],
-//         pieSliceBorderColor: '#29303A',
-//         pieSliceText: 'none',
-//         width: '100%',
-//         height: '100%',
-//         enableInteractivity: false,
-//         selectionMode: 'multiple',
-//         tooltip: {
-//           trigger: 'selection',
-//           isHtml: true,
-//           text: 'percentage',
-//           ignoreBounds: true,
-//         },
-//         pieStartAngle: 110
-//     }
-//     var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-
-//     google.visualization.events.addListener(chart, 'ready', function(e) {
-//         var selected_rows = [];
-//         for (var i = 0; i < 3 - 1; i++) {
-//           selected_rows.push({row: i, column: null});
-//         }
-//         chart.setSelection(selected_rows);
-//       });
-
-//     chart.draw(data, options);
-// }
-
 const expand = (type) => {
     if (type === 'designer') {
         const tooltip = document.querySelector('#designer-tooltip');
@@ -179,6 +91,8 @@ const card = carousel.querySelector(".card");
 const cards = carousel.querySelectorAll(".card");
 const leftButton = document.querySelector(".left-slide");
 const rightButton = document.querySelector(".right-slide");
+const mobileLeftButton = document.querySelector(".mobile-left-btn");
+const mobileRightButton = document.querySelector(".mobile-right-btn");
 const dotsContainer = document.querySelector('.dots');
 const cardCount = carousel.querySelectorAll(".card").length;
 let offset = 0;
@@ -250,26 +164,5 @@ const handleSwipeBackward = () => {
 // Add the click events
 leftButton.addEventListener("click", handleSwipeBackward)
 rightButton.addEventListener("click", handleSwipeForward)
-
-// swipe left & right for mobile
-carousel.addEventListener("touchstart", () => {
-    touchstartX = event.screenX;
-    touchstartY = event.screenY;
-})
-
-carousel.addEventListener('touchend', function(event) {
-    touchendX = event.screenX;
-    touchendY = event.screenY;
-    handleGesture();
-}, false); 
-
-function handleGesture() {
-    if (touchendX < touchstartX) {
-        handleSwipeForward();
-    }
-    if (touchendX > touchstartX) {
-        handleSwipeBackward();
-    } else {
-      return;  
-    }
-}
+mobileLeftButton.addEventListener("click", handleSwipeBackward)
+mobileRightButton.addEventListener("click", handleSwipeForward)
